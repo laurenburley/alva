b56bd82bd38c8bc83b8bb6b6e0f79e3875e9fea3
jumace <julian.cebulla@sinnerschrader.com>
2018-04-09 17:45:30 +0200

    feat(wip): some new stuff bla bla

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	transform: {
 		"^.+\\.tsx?$": "ts-jest"
 	},
-	testRegex: "/tests/.*\.[t]sx?$",
+	testRegex: "\/tests\/[^\.]*\.tsx?$",
 	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
 	roots: ["src"],
 	testEnvironment: "node"
